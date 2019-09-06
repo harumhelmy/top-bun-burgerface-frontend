39eada24cd4a5f870af275c79bd44341f231575b
Harum Helmy <harumendhah@gmail.com>
2019-09-06 14:40:06 -0400

    updated images

--- src/Components/Ingredients.js
+++ src/Components/Ingredients.js
@@ -1,8 +1,37 @@
 import React from 'react'
 
+const ingredients = [
+  {
+    name: "patty",
+    img_url: "../images/patty.png"
+  },
+  {
+    name: "tomato",
+    img_url: "../images/tomato.png"
+  },
+  {
+    name: "lettuce",
+    img_url: "../images/lettuce.png"
+  },
+  {
+    name: "pickles",
+    img_url: "../images/pickles.png"
+  }
+]
 
 const Ingredients= () =>{
-
+  return (
+    <div>
+      {
+        ingredients.map( ingr => 
+          <div>
+            {ingr.name} 
+            <img src={(require(`${ingr.img_url}`))} />
+          </div> 
+        )
+      }
+    </div>
+  )
 }
 
 
