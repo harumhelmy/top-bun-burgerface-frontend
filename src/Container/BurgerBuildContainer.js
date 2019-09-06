a884811afffa11cd98f5277c05fc8dfb343dc7d6
Trevahr Robertson <Trevahr@gmail.com>
2019-09-06 19:23:16 -0400

    wow, so many edge cases

--- src/Container/BurgerBuildContainer.js
+++ src/Container/BurgerBuildContainer.js
@@ -1,35 +1,31 @@
-import React from 'react'
+import React from "react";
 
-
-export default class BurgerBuildContainer extends React.Component{
-
-        reverseBurger2 = () => {
-            let reverseBurger2 = this.props.reverseBurger.reverse()
-           
-            return reverseBurger2
-        }
-
-    render(){
-        return(
-            <div>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>{this.reverseBurger2().map(ingredient => <p key={ingredient.name}><img src={require(`../images/${ingredient.name}.png`)}/></p>)}
-        <br/>
-        <img src={require('../images/bottom_bun.png')}/>
-            </div>
-        )
-    }
-
+export default class BurgerBuildContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        {this.props.burger.reverse().map(ingredient => (
+          <p key={Math.floor(Math.random() * 1000000) + 1}>
+            <img src={require(`../images/${ingredient.name}.png`)} />
+          </p>
+        ))}
+        <br />
+        <img src={require("../images/bottom_bun.png")} />
+      </div>
+    );
+  }
 }
--- src/Container/GameContainer.js
+++ src/Container/GameContainer.js
@@ -3,70 +3,78 @@ import BurgerBuildContainer from "./BurgerBuildContainer";
 import Ingredients from "../Components/Ingredients.js";
 
 const orders = {
-  one: ["patty", "tomato", "lettuce", "pickles"],
-  two: ["lettuce", "tomato", "patty", "pickles"],
-  three: ["pickles", "tomato", "lettuce", "patty"],
-  four: ["patty", "pickles", "lettuce", "tomato"]
+  1: ["patty", "tomato", "lettuce", "pickles"],
+  2: ["lettuce", "tomato", "patty", "pickles"],
+  3: ["pickles", "tomato", "lettuce", "patty"],
+  4: ["patty", "pickles", "lettuce", "tomato"]
 };
 
 export default class GameContainer extends React.Component {
-  
   constructor() {
-      super();
-      this.state = {
-          currentBurger: [],
-          currentBurger2: [],
-          currentOrderNumbers: [],
-          currentScore: 0
-      };
+    super();
+    this.state = {
+      currentBurger: [],
+      currentBurger2: [],
+      currentOrderNumber: 1,
+      currentScore: 0,
+      clickCounter: 0
+    };
   }
 
-  buildBurger = (ingr) => {
+  buildBurger = ingr => {
+    console.log("hello");
 
-    this.setState({
-      currentBurger: [...this.state.currentBurger, ingr],
-      currentBurger2: [...this.state.currentBurger, ingr]
-    })
-  }
+
+    if (this.state.clickCounter < 5) {
+       this.setState({
+          currentBurger: [...this.state.currentBurger, ingr],
+          currentBurger2: [...this.state.currentBurger, ingr],
+          clickCounter: this.state.clickCounter + 1
+        })
+    } else {
+      alert("stahp");
+      this.setState({
+        currentBurger: [],
+        currentBurger2: [],
+        clickCounter: 0
+      })
+    }
+  };
 
   burgerSubmit = () => {
-    console.log('clicked the bun')
+    console.log("clicked the bun");
 
-    let results = []
+    let results = [];
 
-    for (let i=0; i < this.state.currentBurger.length; i++){
-      if( this.state.currentBurger[i].name !== orders.one[i] ){
-        results.push(false) 
-      } 
+    for (let i = 0; i < this.state.currentBurger.length; i++) {
+      if (this.state.currentBurger[i].name !== orders[this.state.currentOrderNumber][i]) {
+        results.push(false);
+      }
     }
 
-     results.length > 0 ? console.log('bye') : this.setState({ currentScore: this.state.currentScore + 1 })
-  
-     this.setState({
-       currentBurger: [],
-       currentBurger2: []
-     })
-     
-  }
+    results.includes(false)
+      ? console.log(results)
+      : this.setState({ currentScore: this.state.currentScore + 1 });
 
-  reverseBurger = () =>{
-    let completedBurger = this.state.currentBurger2
-    return completedBurger
-  }
-        
-    // console.log('did the burger')
-    // this.setState({
-    //   currentScore: this.state.currentScore + 1,
-    //   currentBurger: []
+    this.setState({
+      currentBurger: [],
+      currentBurger2: [],
+      currentOrderNumber: Math.floor(Math.random() * 4) + 1,
+      clickCounter: 0
+    });
+  };
 
   render() {
     return (
       <div>
-        <Ingredients buildBurger={this.buildBurger}
+        <Ingredients
+          buildBurger={this.buildBurger}
           burgerSubmit={this.burgerSubmit}
         />
-        <BurgerBuildContainer reverseBurger={this.reverseBurger()} orders={orders} />
-
+        <BurgerBuildContainer
+          burger={this.state.currentBurger2}
+          orders={orders}
+        />
       </div>
     );
   }
