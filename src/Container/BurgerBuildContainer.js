e95ecf49fc1119ae516b48a6e81fb55a5f59020f
Harum Helmy <harumendhah@gmail.com>
2019-09-09 15:13:57 -0400

    still need to figure out styling oof

--- src/Container/BurgerBuildContainer.js
+++ src/Container/BurgerBuildContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 export default class BurgerBuildContainer extends React.Component {
 
@@ -18,30 +18,36 @@ export default class BurgerBuildContainer extends React.Component {
     // }
     
     return (
-      <div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        { this.props.burger.reverse().map( (ingredient, index) => (
-          <p key={Math.floor(Math.random() * 1000000) + 1}>
-            <img style={{margin: '-45px', zIndex: `-${index}`, position: "relative"}}
-              src={require(`../images/${ingredient.name}.png`)} />
-          </p>
-        ) ) }
-        <br />
-        <img src={require("../images/bottom_bun.png")} />
-      </div>
+      <Fragment> 
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        <div style={{height: '200px', position: 'relative'}}>
+          
+          { this.props.burger.reverse().map( (ingredient, index) => (
+            <p key={Math.floor(Math.random() * 1000000) + 1}
+              style={{top: `${180 - index * 10}`, position: 'aboslute'}}
+            >
+              <img style={{margin: '-45px', zIndex: `-${index}`, position: 'relative'}}
+                src={require(`../images/${ingredient.name}.png`)} />
+            </p>
+          ) ) }
+          <br />
+        </div>
+
+        
+
+        <div style={{position: 'relative', zIndex: '-10'}}>
+          <img src={require("../images/bottom_bun.png")} />
+        </div>
+
+      </Fragment>
     );
   }
 }
