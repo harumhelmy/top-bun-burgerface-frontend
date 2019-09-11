223568fdb5fd8103671bca42f8008e1874117e3f
Harum Helmy <harumendhah@gmail.com>
2019-09-11 18:55:31 -0400

    babel why again

--- src/Components/Customers.js
+++ src/Components/Customers.js
@@ -8,9 +8,10 @@ export default class Customers extends React.Component {
     render(){
   
       return(
-            <div style={{position: 'relative', display: "flex", flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'center'}} onClick={() => this.props.selectOrder(this.props.orderNumber)} >
-                <div style={{position: 'relative', display: "flex", flexDirection: 'column', justifyContent: 'flex-end' }}>
+            <div style={{position: 'relative', display: "flex", flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'center'}} 
+                onClick={() => this.props.selectOrder(this.props.orderNumber)} >
 
+                <div style={{position: 'relative', display: "flex", flexDirection: 'column', justifyContent: 'flex-end' }}>
 
                         {this.props.order.slice(0).reverse().map( (ingr, index) => 
                             <p style={{position: 'relative', 
@@ -31,13 +32,12 @@ export default class Customers extends React.Component {
                                     </img>
                             </p> 
                         )}
+
                 </div>
-
-
-            {/* {this.props.order.map( ingr =>  <div style={{position: 'relative', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-end'}} key={Math.floor(Math.random() * 1000000) + 1}>{ingr}</div> )} */}
+                
                 <img margin='-100px' alt='' height="20%" width="20%"src='https://thenypost.files.wordpress.com/2016/09/tv_simpsons_homer1-1a.jpg?quality=90&strip=all&strip=all'/>
             
-                </div>
+            </div>
         )
     }
 
