bb68fb08cbd576a0f62826aeefd780a4b19eb311
Trevahr Robertson <Trevahr@gmail.com>
2019-09-11 18:06:52 -0400

    merging height change

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -5,11 +5,7 @@ export default class Timer extends React.Component {
   constructor(){
     super()
     this.state = {
-<<<<<<< HEAD
-      gameTimer: 50
-=======
       gameTimer: 30
->>>>>>> styling-forever
     }
   }
   
